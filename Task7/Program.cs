113d6d38b5720ff0ec3074c9566449d56c3b5360
Aleksey Khorkov <60649756+LSubarin@users.noreply.github.com>
2023-03-05 18:07:26 +0300

    Task7. Последняя цифра введенного числа. % - Остаток от деления.

---
+++ Task7/Program.cs
@@ -0,0 +1,5 @@
+﻿// Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+System.Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(num + "->" + num % 10);
