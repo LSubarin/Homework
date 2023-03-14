cc4f1b743df1ca7b6247e225b4c7d6091cbfbce0
Aleksey Khorkov <60649756+LSubarin@users.noreply.github.com>
2023-03-14 12:33:28 +0300

    Задача №23. На входе N на выходе таблица кубов. Math.Abs Math.Pow

---
+++ Task23/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача №23. Напишите программу которая принимает на вход число (N)
+// и выдает таблицу кубов чисел от 1 до N.
+
+System.Console.WriteLine("введите число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine())); // Math.Abs - делает из отрицательного числа положительное
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write(Math.Pow(i, 3) + " ");
+}
